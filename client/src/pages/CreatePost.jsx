cc41ee9f0cebd48a9f19230c106319d5f5207662
Handemore7 <handemore7@gmail.com>
2023-01-23 00:44:20 -0500

    Server setup, routes but only /dallE works

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -15,8 +15,31 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false)
   const [loading, setLoading] = useState(false)
 
-  const generateImage = () => {
-    
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch('http://localhost:8080/api/v1/dallE', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ prompt: form.prompt })
+        })
+
+        const data = await response.json();
+
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}`})
+
+      } catch (error) {
+        console.log(error);
+        alert('error: ' + error)
+      } finally {
+        setGeneratingImg(false)
+      }
+    } else {
+      alert('please enter a prompt')
+    }
   }
 
   const handleSubmit = () => {
